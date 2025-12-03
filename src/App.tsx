b2ed9dd5d6ab15124cb3f78e19c0da3057e18a86
Shrutakirti Chakraborty <77294013+officialshrutakirti94@users.noreply.github.com>
2025-12-03 16:05:56 +0530

    just chill3

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ import { Profile } from './components/profile/Profile';
 
 const AppContent: React.FC = () => {
   const { state } = useApp();
-// egtter chuda saka
+// egtter chuda saka paka lala
   const renderPage = () => {
     switch (state.currentPage) {
       case 'login':
